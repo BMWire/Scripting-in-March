00d58d99a29a9657b0c63d12756e9fe3892905b6
BMWire <bmwemawire@gmail.com>
2021-03-16 23:24:59 +0300

    Addition of Nested Ifs

--- JS/Mar_15_If.js
+++ JS/Mar_15_If.js
@@ -1,8 +1,10 @@
 /*
 It covers:
     If Statements.
+    Nested Ifs
 */
 
+//Normal Ifs
 var popeHome = prompt("What is the home city of the pope?");
 if (popeHome === "Vatican") 
 {
@@ -11,4 +13,33 @@ if (popeHome === "Vatican")
 else
 {
     alert("Enda soma World History.");
+}
+
+// Nessted Ifs
+if (c === d)
+{
+    if (x === y) 
+    {
+    g = h;
+    } // If c = d AND x = y; g = h.
+    else if (a === b) 
+    {
+    g = h;
+    } // If c = d AND a = b; g = h.
+    else {
+    e = f;
+    } //Otherwise, e = f. Otherwise ya x != y or a != b.
+    }
+    else {
+    e = f;
+    // Otherwise ya c != d
+}
+
+if ((x === y || a === b) && c === d) // If x = y, OR a = b AND c = d; g = h. Otherwise, e = f. 
+{
+    g = h;
+}
+else 
+{
+    e = f;
 }
