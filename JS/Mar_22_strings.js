9b496ac3098db997780c70659f76f514f85255b0
BMWire <bmwemawire@gmail.com>
2021-03-22 21:39:47 +0300

    Continued with strings

---
+++ JS/Mar_22_strings.js
@@ -0,0 +1,19 @@
+/*
+This contains:
+    A better way of finding strings and replacing them wit others.
+    string.indexOf
+*/
+
+var bigText = "It is startling to think that, even in the darkest depths of World War II, J. R. R. Tolkien was writing the trilogy, which contains, with the weird applicability available only to poetry and myth, the essential notion that the good gray wizard can understand the evil magic precisely because he is just enough like them to grasp their minds and motives in ways they cannot grasp his.";
+document.write(bigText);
+
+var firstChar = bigText.indexOf("World War II");
+for (let i = 0; i < bigText.length; i++) 
+{
+    if (firstChar !== -1) 
+    {
+        bigText = bigText.slice(0, firstChar) + "the Second World War" + bigText.slice(firstChar + 12);
+    }
+    
+}
+document.write(bigText);
