d7acfedb7baadfbe6c64a54592d84fc0588bbe52
BMWire <bmwemawire@gmail.com>
2021-03-24 21:02:26 +0300

    Strings: Finding a character at a location. Modified Indexof of Mar22.

---
+++ JS/Mar_24_Strings.js
@@ -0,0 +1,20 @@
+/*
+This contains:
+    Finding a Character at a Location
+*/
+var mainWord = "Lovely";
+var firstChar = mainWord.charAt(0);
+var lastChar = mainWord.charAt(length - 1);
+
+alert(mainWord + "<br>" + firstChar + "<br>" + lastChar);
+
+//Looks for an exclamation mark
+var seekWord = "I love my girl!!!";
+for (let i = 0; i < seekWord.length; i++) {
+    if (seekWord[i] === "!")
+    {
+        alert("Exclamation mark found!");
+        break;    
+    }
+    
+}
