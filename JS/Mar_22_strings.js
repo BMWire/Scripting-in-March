e717ab416487895105c6703d2a95a3c94525c803
BMWire <bmwemawire@gmail.com>
2021-03-25 14:43:42 +0300

    Added CSS, Learned replace()

--- JS/Mar_22_strings.js
+++ JS/Mar_22_strings.js
@@ -3,10 +3,11 @@ This contains:
     A better way of finding strings and replacing them with others.
     string.
     indexOf
+    replace
 */
 
 var bigText = "It is startling to think that, even in the darkest depths of World War II, J. R. R. Tolkien was writing the trilogy, which contains, with the weird applicability available only to poetry and myth, the essential notion that the good gray wizard can understand the evil magic precisely because he is just enough like them to grasp their minds and motives in ways they cannot grasp his.";
-document.write(bigText);
+alert(bigText);
 
 var majorNumber = bigText.indexOf("World War II");
 for (let i = 0; i < bigText.length; i++) 
@@ -17,4 +18,10 @@ for (let i = 0; i < bigText.length; i++)
     }
     
 }
-document.write(bigText);
+alert(bigText);
+
+
+//Searching and replacing using replace()
+var newBig = (bigText.replace("World War II", "the Second World War updated by replace!"));
+
+alert(newBig);
