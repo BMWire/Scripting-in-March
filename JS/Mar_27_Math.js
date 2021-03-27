9d9fcdaec79270f97d6a62e8a07aa67ef15e30af
BMWire <bmwemawire@gmail.com>
2021-03-27 19:50:39 +0300

    Started Mathematical Operations

---
+++ JS/Mar_27_Math.js
@@ -0,0 +1,21 @@
+/*
+This contains:
+    Math.x functions
+        Math.round
+        Math.ceil(Forcefully rounds the number to the upper limit)
+        Math.floor(Forcefully rounds the number to the lower limit)
+        Math.random 
+*/
+var sizeKenya = 544899.456;
+alert("The average size of the African country of Kenya is:" + Math.round(sizeKenya));
+
+//Generating random numbers between 1 and 8
+var numberOfStars;
+
+for (let i = 1; i < 8; i++) 
+{
+    var bigDecimal = Math.random();
+    var improvedNum = (bigDecimal * 8) + 1;
+    var numberOfStars = Math.floor(improvedNum);    
+    alert(numberOfStars);
+}
---
+++ JS/Mar_27_MoreMath.js
@@ -0,0 +1,8 @@
+/*
+This contains:
+    More math operations
+    Learning how JS handles and switches between strings and numbers
+*/
+
+var profit = "200" - "30";
+alert(profit);
