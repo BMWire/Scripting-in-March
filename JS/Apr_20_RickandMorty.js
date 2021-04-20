503323b5c8d61b44f731cbb0491e88184acef476
BMWire <bmwemawire@gmail.com>
2021-04-20 08:57:28 +0300

    .

--- JS/Apr_20_RickandMorty.js
+++ JS/Apr_20_RickandMorty.js
@@ -1,11 +1,14 @@
 const seasons = ["S01", "S02", "S03", "S04"];
 const episodes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+const dom = document.getElementById("thisEpisode");
+
 function episodeChooser ()
 {
     let chosen = "";    
     const ranEpisode = Math.floor(Math.random()*episodes.length);
     const ranSeason = Math.floor(Math.random()*seasons.length);
-    chosen = seasons[ranSeason] + "." + ranEpisode;
+    chosen = seasons[ranSeason] + "." + episodes[ranEpisode];
+    dom.textContent = chosen; 
     console.log(chosen);
 }
 
