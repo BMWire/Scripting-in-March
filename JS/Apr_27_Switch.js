23381687cf3c21e43e8a3a26a68e03378dbf9956
BMWire <bmwemawire@gmail.com>
2021-04-27 22:50:29 +0300

    Added Switch

---
+++ JS/Apr_27_Switch.js
@@ -0,0 +1,24 @@
+/*Self explanatory. This one deals with:
+    Flow control - Switch Case
+*/
+
+var dayofWeek = Sun;
+
+switch (dayofWeek)
+{
+    case "Sun":
+        alert("Whopeee");
+        break;
+
+    case "Sat":
+        alert("Whopee");
+        break;
+
+    case "Fri":
+        alert("TGIF");
+        break;
+
+    default:
+        alert("Shoot me now");
+        break;
+}
