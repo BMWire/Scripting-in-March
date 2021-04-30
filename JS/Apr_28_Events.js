27e2074a8b8d13678ccfb85b53c8a6c8824689f3
BMWire <bmwemawire@gmail.com>
2021-04-30 12:14:07 +0300

    Getting data from fields

--- JS/Apr_28_Events.js
+++ JS/Apr_28_Events.js
@@ -2,3 +2,15 @@
 This one contains:
     Inline Event Handlers - In the HTML Document
 */
+//Learnt onFocus and onBlur - Fields
+//Learnt getElementById - Getting data from fields 
+
+function checkAddress(fieldId)
+{
+    var val = document.getElementById(fieldId).value;
+
+    if (val === "")
+    {
+        alert("The email address you entered is invalid");
+    }
+}
