5b05a24ce15c4dad867ab57ea20f452447089ddf
BMWire <bmwemawire@gmail.com>
2021-04-30 13:10:12 +0300

    .innerHTML

--- JS/Apr_28_Events.js
+++ JS/Apr_28_Events.js
@@ -4,6 +4,7 @@ This one contains:
 */
 //Learnt onFocus and onBlur - Fields
 //Learnt getElementById - Getting data from fields 
+//Learnt .innerHTML - Giving data to the DOM elements
 
 function checkAddress(fieldId)
 {
@@ -14,3 +15,6 @@ function checkAddress(fieldId)
         alert("The email address you entered is invalid");
     }
 }
+
+var par = document.getElementById('writeTry');
+par.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ducimus sed tenetur dignissimos non fuga, laudantium consequuntur atque adipisci quo!"
