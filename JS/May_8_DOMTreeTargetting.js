dad80dd9325c256aede2b2176a5b07e41ea5ff88
BMWire <bmwemawire@gmail.com>
2021-05-11 00:08:28 +0300

    Adding DOM elements

--- JS/May_8_DOMTreeTargetting.js
+++ JS/May_8_DOMTreeTargetting.js
@@ -2,12 +2,14 @@
 This one contains:
     1. targetting elements in the DOM using the names of their parents or their number in the family hierarchy.
     2. finding out the attributes of nodes and manipulating those.
+    3. adding new node to the DOM
 */
 //Finding nodes in the DOM using their number in the family hierarchy
 let newP = '';
 var parentDiv = document.getElementById('motherDiv');
 var firstKid = parentDiv.childNodes[0];
 var midChild = firstKid.nextSibling;
+var lastKid = parentDiv.lastChild;
 
 
 newP += '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit numquam nisi libero magnam mollitia. Aliquid possimus fuga placeat voluptas! Itaque quae reprehenderit praesentium cupiditate saepe omnis error iusto, animi repudiandae.</p>';
@@ -31,3 +33,8 @@ else
     midChild.setAttribute('class', 'embedded');
     console.log(midChild.getAttribute('class'));
 }
+
+//Adding a new node to the DOM
+var lastPar = document.createElement('p');
+lastKid.appendChild(lastPar);
+lastPar.innerHTML = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis velit id earum laboriosam rerum nihil beatae sint neque eaque. Voluptas repudiandae neque, facere ut dolor voluptates soluta odio laboriosam similique laborum sunt fugit, placeat maxime. Ullam perspiciatis debitis facere possimus!';
