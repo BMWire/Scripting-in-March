b31c23e0d1bdd7a5f17fd49da0d4e8e1d6572e0c
BMWire <bmwemawire@gmail.com>
2021-05-19 16:17:24 +0300

    Browser Control: Getting URL Start

---
+++ JS/May_19_BrowserControl.js
@@ -0,0 +1,9 @@
+/*
+    This one contains Browser Control measures
+*/
+
+var whereWeAt = window.location.href;
+console.log(whereWeAt);
+
+var theDomain =  window.location.hostname;
+console.log(theDomain);
